98d64f4ec5039689f0ef992912b939a5a88ce407
sangki-song <ihatenullpointr@gmail.com>
2023-05-15 14:07:24 +0900

    Initial commit

---
+++ app/src/main/java/org/techtown/notificationtrigger/MainActivity.java
@@ -0,0 +1,130 @@
+package org.techtown.notificationtrigger;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+    }
+
+
+    public void onButton2Clicked (View view) {
+        NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Notification 객체를 생성해주는 건축가객체 생성(AlertDialog 와 비슷)
+        NotificationCompat.Builder builder= null;
+
+        //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            String channelID="channel_01"; //알림채널 식별자
+            String channelName="MyChannel01"; //알림채널의 이름(별명)
+
+            //알림채널 객체 만들기
+            NotificationChannel channel= new NotificationChannel(channelID,channelName,NotificationManager.IMPORTANCE_DEFAULT);
+
+            //알림매니저에게 채널 객체의 생성을 요청
+            notificationManager.createNotificationChannel(channel);
+
+            //알림건축가 객체 생성
+            builder=new NotificationCompat.Builder(this, channelID);
+
+
+        }else{
+            //알림 건축가 객체 생성
+            builder= new NotificationCompat.Builder(this);
+        }
+
+        //건축가에게 원하는 알림의 설정작업
+        builder.setSmallIcon(android.R.drawable.ic_menu_view);
+
+        //상태바를 드래그하여 아래로 내리면 보이는
+        //알림창(확장 상태바)의 설정
+        builder.setContentTitle("더미알림");//알림창 제목
+        builder.setContentText("알림 구분을 확인하기 위한 샘플");//알림창 내용
+        //알림창의 큰 이미지
+        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.ic_baseline_done_outline_24);
+        builder.setLargeIcon(bm);//매개변수가 Bitmap을 줘야한다.
+
+        //건축가에게 알림 객체 생성하도록
+        Notification notification=builder.build();
+
+        //알림매니저에게 알림(Notify) 요청
+        notificationManager.notify(1, notification);
+        Log.d(">>>알림 생성", "더미알림");
+
+    }
+
+    public void ButtonClicked1(View view) {
+
+        //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+        NotificationManager notificationManager=(NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Notification 객체를 생성해주는 건축가객체 생성(AlertDialog 와 비슷)
+        NotificationCompat.Builder builder= null;
+
+        //Oreo 버전(API26 버전)이상에서는 알림시에 NotificationChannel 이라는 개념이 필수 구성요소가 됨.
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+
+            String channelID="channel_01"; //알림채널 식별자
+            String channelName="MyChannel01"; //알림채널의 이름(별명)
+
+            //알림채널 객체 만들기
+            NotificationChannel channel= new NotificationChannel(channelID,channelName,NotificationManager.IMPORTANCE_DEFAULT);
+
+            //알림매니저에게 채널 객체의 생성을 요청
+            notificationManager.createNotificationChannel(channel);
+
+            //알림건축가 객체 생성
+            builder=new NotificationCompat.Builder(this, channelID);
+
+
+        }else{
+            //알림 건축가 객체 생성
+            builder= new NotificationCompat.Builder(this);
+        }
+
+        //건축가에게 원하는 알림의 설정작업
+        builder.setSmallIcon(R.drawable.ic_baseline_done_outline_24);
+
+        //상태바를 드래그하여 아래로 내리면 보이는
+        //알림창(확장 상태바)의 설정
+        builder.setContentTitle("알림생성");//알림창 제목
+        builder.setContentText("알림 인식을 확인하기 위한 샘플");//알림창 내용
+        //알림창의 큰 이미지
+        Bitmap bm= BitmapFactory.decodeResource(getResources(),R.drawable.ic_baseline_done_outline_24);
+        builder.setLargeIcon(bm);//매개변수가 Bitmap을 줘야한다.
+
+        //건축가에게 알림 객체 생성하도록
+        Notification notification=builder.build();
+
+        //알림매니저에게 알림(Notify) 요청
+        notificationManager.notify(1, notification);
+        Log.d(">>>알림 생성", "타겟알림");
+
+        //알림 요청시에 사용한 번호를 알림제거 할 수 있음.
+        //notificationManager.cancel(1);
+
+    }
+}
